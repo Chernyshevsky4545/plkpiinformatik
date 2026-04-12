c5b766b3e4e374f3944c532a21c80c6d3e98bade
Chernyshevskiy Nikita <cernysevskijnikita875@gmail.com>
2026-04-12 23:07:57 +0300

    123

---
+++ 904/task5/task5/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("task5")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ZverDVD")]
+[assembly: AssemblyProduct("task5")]
+[assembly: AssemblyCopyright("Copyright © ZverDVD 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("684b2ff8-ecda-42b4-ac51-6e2c2e6b0a9f")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 904/task8/task8/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            player.Top -= 10;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            player.Left -= 10;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            player.Top += 10;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            player.Left += 10;
+        }
+    }
+}
