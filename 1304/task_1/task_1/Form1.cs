7c08f33e69c42d4f5d6bd3243afd6e0fa4533543
Chernyshevskiy Nikita <cernysevskijnikita875@gmail.com>
2026-04-19 19:06:58 +0300

    55555

---
+++ 1304/task_1/task_1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace task_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            button1.MouseEnter += Button1_MouseEnter;
+        }
+
+        private void Button1_MouseEnter(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            int newX = random.Next(0, this.ClientSize.Width - button1.Width);
+            int newY = random.Next(0, this.ClientSize.Height - button1.Height);
+
+
+            button1.Location = new Point(newX, newY);
+        }
+    }
+}
